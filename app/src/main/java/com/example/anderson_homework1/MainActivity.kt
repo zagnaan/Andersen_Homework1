677af772fcf76d3b5f9f1a12ada98b67bac0ebdf
Shynggys Samarkhanov <samarkhanovshyngys@gmail.com>
2023-12-04 14:49:38 +0100

    Initial commit

---
+++ app/src/main/java/com/example/anderson_homework1/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.anderson_homework1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.anderson_homework1.ui.theme.Anderson_Homework1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Anderson_Homework1Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Surface(color = Color.Magenta) {
+        Text(
+            text = "Hi, my name is  $name!",
+            modifier = modifier.padding(24.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Anderson_Homework1Theme {
+        Greeting("Shynggys")
+    }
+}
